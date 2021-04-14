4f44c8a1af96b3b6c1606ddcbe55bc148195f651
Bianca Aguglia <bianca.aguglia@gmail.com>
2021-04-14 18:17:04 -0400

    add to NMC_webdev_mozilla_learning_path 02_introduction_to_html

--- NMC_webdev_mozilla_learning_path/02_introduction_to_html/mini_project_02_02_head_example/script.js
+++ NMC_webdev_mozilla_learning_path/02_introduction_to_html/mini_project_02_02_head_example/script.js
@@ -1,17 +1,18 @@
 const list = document.createElement('ul');
 const html = document.querySelector('html');
 const container = document.querySelector('.container')
+
 container.appendChild(list);
 
 html.onclick = function() {
   const listItem = document.createElement('li');
-  const listContent = prompt('What content do you want the list item to have?');
+  const listContent = prompt('Enter your answer');
   listItem.textContent = listContent;
   list.appendChild(listItem);
 
   listItem.onclick = function(e) {
     e.stopPropagation();
-    const listContent = prompt('Enter new content for your list item');
+    const listContent = prompt('Enter your updated answer');
     this.textContent = listContent;
   }
 }
