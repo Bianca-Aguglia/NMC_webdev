4a9bfad1e0a12ab1b7d56298ffa0c2f1515e912f
Bianca Aguglia <bianca.aguglia@gmail.com>
2021-04-13 22:11:31 -0400

    update NMC_webdev_mozilla_learning_path

---
+++ NMC_webdev_mozilla_learning_path/02_introduction_to_html/mini_project_02_02_meta_example/script.js
@@ -0,0 +1,17 @@
+const list = document.createElement('ul');
+const html = document.querySelector('html');
+const container = document.querySelector('.container')
+container.appendChild(list);
+
+html.onclick = function() {
+  const listItem = document.createElement('li');
+  const listContent = prompt('What content do you want the list item to have?');
+  listItem.textContent = listContent;
+  list.appendChild(listItem);
+
+  listItem.onclick = function(e) {
+    e.stopPropagation();
+    const listContent = prompt('Enter new content for your list item');
+    this.textContent = listContent;
+  }
+}
---
+++ NMC_webdev_mozilla_learning_path/README.md
@@ -0,0 +1,8 @@
+# Mozilla Learning Path
+
+This is a collection of notes I take and mini-projects I work on while following the [Mozilla web development learning path](https://developer.mozilla.org/en-US/docs/Learn).
+
+TO-DO:
+- [ ] 01_getting_started_with_the_web
+    - [ ] alternating indents to the poem
+    - [ ] button to change pictures
